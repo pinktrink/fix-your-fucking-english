b0150718a8938de8fadf8e34ccb774f214af9749
Eric Kever <lolbummer@gmail.com>
2013-07-29 15:56:38 -0700

    Raise Http404 on bad lookup.

--- fyfe/views/word.py
+++ fyfe/views/word.py
@@ -1,8 +1,13 @@
+from django.http import Http404
+
 from fyfe.models import Search
 from fyfe.views.utils import renderWordPage
 
 def word(request, werd):
-	search = Search.objects.get(word=werd)
-	fix = search.fix
+    try:
+    	search = Search.objects.get(word=werd)
+    	fix = search.fix
+    except Search.DoesNotExist:
+        raise Http404
 	
 	return renderWordPage(request, fix)
