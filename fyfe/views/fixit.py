035bf5b117d66cc71816f9040bd161b7c1257d04
Eric Kever <lolbummer@gmail.com>
2013-07-29 18:32:48 -0700

    Revamp fixit view, DRY.

--- fyfe/views/fixit.py
+++ fyfe/views/fixit.py
@@ -1,5 +1,7 @@
 import re
 
+from urlparse import unquote
+
 from django.shortcuts import render
 
 from fyfe.models import Search
@@ -7,54 +9,40 @@ from fyfe.views.utils import renderDescription
 
 
 def fixit(request, werd, sentance):
-    if werd.find(':') is not -1:
-        described = []
+    regex = re.compile(r':(.*?):')
+    
+    described = []
+    
+    descriptions = []
+    
+    words = filter(None, werd.split(':'))
+    
+    incorrect = correct = re.sub(r'(^|[^\+])(\+)($|[^\+])', r'\1 \3', sentance)
+    
+    for word in words:
+        correct = unquote(re.sub(regex, '<em>' + word + '</em>', correct, 1))
+        incorrect = unquote(re.sub(regex, r'<em>\1</em>', incorrect, 1))
         
-        descriptions = []
-        
-        words = werd.split(':')
-        
-        incorrect = correct = sentance
-        
-        for word in words:
-            correct = re.sub(r':(.*?):', '<em>' + word + '</em>', correct, 1)
-            incorrect = re.sub(r':(.*?):', r'<em>\1</em>', incorrect, 1)
-            
-            if word not in described:
-                try:
-                    searchFix = Search.objects.get(word=word).fix
-                    
-                    allWords = [i.word for i in searchFix.search_set.all()]
-                    
-                    if not searchFix.supress_fixit_explanation:
-                        descriptions.append(renderDescription(
-                            searchFix.description,
-                            allWords
-                        ))
-                    
-                    described.extend(allWords)
+        if word not in described:
+            try:
+                searchFix = Search.objects.get(word=word).fix
                 
-                except Search.DoesNotExist:
-                    pass
-                    
-                    described.append(word)
-        
-        description = '<br /><hr /><br />'.join(descriptions)
-        
-    else:
-        correct = re.sub(r':(.*?):', '<em>' + werd + '</em>', sentance)
-        incorrect = re.sub(r':(.*?):', r'<em>\1</em>', sentance)
-        
-        try:
-            searchFix = Search.objects.get(word=werd).fix
+                allWords = [i.word for i in searchFix.search_set.all()]
+                
+                if not searchFix.supress_fixit_explanation:
+                    descriptions.append(renderDescription(
+                        searchFix.description,
+                        allWords
+                    ))
+                
+                described.extend(allWords)
             
-            description = renderDescription(
-                searchFix.description,
-                [i.word for i in searchFix.search_set.all()]
-            )
-        
-        except Search.DoesNotExist:
-            description = ""
+            except Search.DoesNotExist:
+                pass
+                
+                described.append(word)
+    
+    description = '<br /><hr /><br />'.join(descriptions)
     
     return render(request, 'fyfe/fixit.html', {
         'correct' : correct,
