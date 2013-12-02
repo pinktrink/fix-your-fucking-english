aabf97aebe625581628aaf68aed68d97d3aaf1df
Eric Kever <lolbummer@gmail.com>
2013-12-02 11:13:44 -0800

    Backtick highlighting.

--- fyfe/views/utils.py
+++ fyfe/views/utils.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 def renderDescription(desc, words):
     desc = re.sub(r'-[\r\n]{1,2}', '<hr />', desc)
     desc = desc.replace('\n', '<br />')
+    desc = re.sub(r'`(.*?)`', r'<em>\1</em>', desc)
 
     for word in words:
         r = re.compile(r'\b(' + word + r')\b', re.IGNORECASE)
